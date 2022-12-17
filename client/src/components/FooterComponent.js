c3d448a703eaf99d00264499f9e744044840d77f
roshan-ican <roshansahani535@gmail.com>
2022-12-17 09:03:31 +0530

    footer added

--- client/src/components/FooterComponent.js
+++ client/src/components/FooterComponent.js
@@ -1,19 +1,18 @@
-import React,{Component} from 'react';
+import React, { Component } from "react"
 
 class Footer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-    constructor(props){
-        super(props);
-        this.state={
-        }
-    }
-
-render(){
-    return(
-                    <p className="footer" align="center">© Copyright 2022 @BookLovers</p>
-          );
+  render() {
+    return (
+      <p className="footer" align="center">
+        © Copyright 2022 @BookLovers
+      </p>
+    )
+  }
 }
 
-}
-
-export default Footer;
+export default Footer
